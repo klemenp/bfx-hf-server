5cefbb1bc3fa0edcab6b65b95aef9c10b14130aa
klemenp <klemen@your.md>
2020-09-25 11:24:22 +0200

    deps fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('bfx-hf-util/lib/catch_uncaught_errors')
 
 const HFDB = require('bfx-hf-models')
 const DataServer = require('bfx-hf-data-server')
-const HFDBLowDBAdapter = require('bfx-hf-models-adapter-low')
+const HFDBLowDBAdapter = require('bfx-hf-models-adapter-lowdb')
 const HFDBSQLAdapter = require('bfx-hf-models-adapter-sql')
 const { schema: HFDBBitfinexSchema } = require('bfx-hf-ext-plugin-bitfinex')
 const { schema: HFDBDummySchema } = require('bfx-hf-ext-plugin-dummy')
